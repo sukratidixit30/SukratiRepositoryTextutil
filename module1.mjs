667e52d3e1f61ef342bfaa0dbe699d659867e525
sukrati <er.sukratidixit30@gmail.com>
2026-02-23 10:58:48 +0530

    export defaut and named module

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import ui, {a, b} from './module2.mjs';
+
+console.log(ui);
+console.log(a);
+console.log(b);
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+const a = "harry";
+const b = "rashmi";
+const c = "sukrati";
+
+export default c;
+export {a,b};
