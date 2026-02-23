ce4619a8997648a399d57d671001e13ef20606b1
sukrati <er.sukratidixit30@gmail.com>
2026-02-23 15:42:31 +0530

    removed about from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,18 +9,18 @@ export default function Navbar( {title = "Guest",
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">{title}</a>
+            <a className="navbar-brand" href="#">{title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
                 </li> 
-                <li className="nav-item">         
+                {/*<li className="nav-item">         
                     <a className="nav-link" aria-current="page" href="/">{aboutUs}</a>
-                </li>
+                </li>*/}
             </ul>
             {/*<form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
